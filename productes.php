a68f95ec427f9434d4d88210811e103baed4282a
jjaviv <javier04.vng@gmail.com>
2024-02-23 17:57:03 +0100

    Co-authored-by: Marko41 <Marko41@users.noreply.github.com>

--- productes.php
+++ productes.php
@@ -12,7 +12,9 @@
         <div class="row" data-aos="fade-up" data-aos-delay="100">
             <div class="col-lg-12 d-flex justify-content-center">
                 <ul id="portfolio-flters">
-                    <li data-filter="*" class="filter-active">TOTS</li>
+                    <li data-filter="*" class="filter-active">Tots</li>
+                    <li data-filter=".filter-app">Teclats</li>
+                    <li data-filter=".filter-card">Auriculars</li>
                     <!--bucle categories-->
                 </ul>
             </div>
