6a0dad99a78f7b6296410c36e06585d57024aae2
Marco <mmacarull@ies-eugeni.cat>
2024-02-23 19:44:15 +0100

    hgsf

--- servei_actualitzar.php
+++ servei_actualitzar.php
@@ -1,21 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="assets/css/style.css" rel="stylesheet">
-    <title>Document</title>
-</head>
-<body>
+<?php include('connexio_woo.php') ?>
+<?php include('header.php') ?><!-- EL Menú està en el header -->
 <?php
-echo '<form method="POST"><div class="row" style="margin-bottom:4px;">;
-echo '<div class="col-md-1 form-group" ><input class="form-control" id="id" name="id" value="'.$post->id.'" readonly type="text"></div>';
+foreach ($woocommerce->get('products') as $post) {
 
-echo <div class="col-md-1 form-group" ><input class="form-control" img-responsive" name="src_image" src="'.$src_image.'" style="width:4vw;" type="image"></div>';
-echo <div class="col-md-1 form-group" ><input class="form-control"
+echo '<form method="POST"><div class="row" style="margin-bottom:4px;">';
+echo '<div class="col-md-1 form-group" ><input class="form-control" id="id" name="id" value="'.$post->id.'" readonly type="text"></div>';
+echo '<div class="col-md-1 form-group" ><input class="form-control" img-responsive" name="src_image" src="'.$src_image.'" style="width:4vw;" type="image"></div>';
 echo '<div class="col-md-4 form-group"><input class="form-control" name="short_description" value="'.strip_tags($post->short_description).'"  type="text"></div>';
-echo '<div class="col-md-1 form-group"><button class="btn center" formaction="seevei_actualitzar_id.php"><i class="bi bi-arrow-clockwise"></i></button></div>';
-echo "</form></div>";
-?>   
-</body>
-</html>
+echo '<div class="col-md-1 form-group"><button class="btn center" formaction="servei_actualitzar_id.php"><i class="bi bi-arrow-clockwise"></i></button></div>';
+echo "</div></form>";
+}
+ include('footer.php') ?>
