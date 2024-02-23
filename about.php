25c1a412ee1f9f4cdc9d948160839a677397c37c
jjaviv <javier04.vng@gmail.com>
2024-02-23 17:37:24 +0100

    ddsaad

--- about.php
+++ about.php
@@ -1,43 +1,38 @@
 <section id="about" class="about section-bg">
-<link href="assets/css/style.css" rel="stylesheet">
-<script src="assets/js/main.js"></script>
-      <div class="container" data-aos="fade-up">
+  <link href="assets/css/style.css" rel="stylesheet">
+  <script src="assets/js/main.js"></script>
+  <div class="container" data-aos="fade-up">
 
-        <div class="section-title">
-          <h2>Nosaltres</h2>
-          <h3>Troba més sobre<span> Nosaltres</span></h3>
-          <p>Som una petita empresa enfocats en l'àmbit informàtic.</p>
-        </div>
+    <div class="section-title">
+      <h2>Nosaltres</h2>
+      <h3>Troba més sobre<span> Nosaltres</span></h3>
+      <p>Som una petita empresa enfocats en l'àmbit informàtic.</p>
+    </div>
 
-        <div class="row">
-          <div class="col-lg-6" data-aos="fade-right" data-aos-delay="100">
-            <img src="assets/img/about.jpg" class="img-fluid" alt="">
-          </div>
-          <div class="col-lg-6 pt-4 pt-lg-0 content d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="100">
-            <h3>A Informàtica Online ens dediquem a vendre tot tipus de productes informàtics per als notres clients</h3>
-            <ul>
-              <li>
-                <i class="bx bx-store-alt"></i>
-                <div>
-                  <h5>Ullamco laboris nisi ut aliquip consequat</h5>
-                  <p>Magni facilis facilis repellendus cum excepturi quaerat praesentium libre trade</p>
-                </div>
-              </li>
-              <li>
-                <i class="bx bx-images"></i>
-                <div>
-                  <h5>Magnam soluta odio exercitationem reprehenderi</h5>
-                  <p>Quo totam dolorum at pariatur aut distinctio dolorum laudantium illo direna pasata redi</p>
-                </div>
-              </li>
-            </ul>
-            <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum
-            </p>
-          </div>
-        </div>
-
+    <div class="row">
+      <div class="col-lg-6" data-aos="fade-right" data-aos-delay="100">
+        <img src="./img/suport.png" class="img-fluid" alt="">
       </div>
-    </section>
+      <div class="col-lg-6 pt-4 pt-lg-0 content d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="100">
+        <h3>A Informàtica Online ens dediquem a vendre tot tipus de productes informàtics per als notres clients.</h3>
+        <ul>
+          <li>
+            <i class="bx bx-store-alt"></i>
+            <div>
+              <h5>Productes!</h5>
+              <p>Tenim disponible una gran varietat de productes per a que puguis escollir el que vulguis.</p>
+            </div>
+          </li>
+          <li>
+            <i class="bx bx-images"></i>
+            <div>
+              <h5>Resol els teus dubtes!</h5>
+              <p>Pots resoldre qualsevol tipus de dubtes de manera online contactant mediant el nostre suport tècnic.</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
+</section>
