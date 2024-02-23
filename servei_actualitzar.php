247824dbd658e6d7069bcdf391d7150206598402
Marco <mmacarull@ies-eugeni.cat>
2024-02-23 19:23:09 +0100

    ffwaf

---
+++ servei_actualitzar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo '<form method="POST"><div class="row" style="margin-bottom:4px;">;
+echo '<div class="col-md-1 form-group" ><input class="form-control" id="id" name="id" value="'.$post->id.'" readonly type="text"></div>';
+
+echo <div class="col-md-1 form-group" ><input class="form-control" img-responsive" name="src_image" src="'.$src_image.'" style="width:4vw;" type="image"></div>';
+echo <div class="col-md-1 form-group" ><input class="form-control"
+
+?>   
+</body>
+</html>
