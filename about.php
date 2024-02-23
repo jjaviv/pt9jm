81e0c6f7b79df45131334647dd260b2acbe82463
jjaviv <javier04.vng@gmail.com>
2024-02-23 17:18:04 +0100

    aaaaaa

--- about.php
+++ about.php
@@ -4,7 +4,7 @@
       <div class="container" data-aos="fade-up">
 
         <div class="section-title">
-          <h2>About</h2>
+          <h2>Nosaltres</h2>
           <h3>Find Out More <span>About Us</span></h3>
           <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
         </div>
